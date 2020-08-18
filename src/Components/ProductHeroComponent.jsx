d85d07df90a2125024b1af5dcc2e08905efc896b
Harsh Verma <verma.harsh98@gmail.com>
2020-08-18 16:39:04 +0530

    Added counter

--- src/Components/ProductHeroComponent.jsx
+++ src/Components/ProductHeroComponent.jsx
@@ -1,54 +1,60 @@
 import React, { Component } from 'react'
 import withCounter from './withCounter';
 
-const ProductHero = () => {
-    return (
-        
-            <div class="main">
-                <div class="img">
-                    <img src="./tyre.jpg" alt="tyre" id="main-img" />
-                </div>
-                <div class="info">
-                    <div class="container">
-                        <div class="heading">
-                            <h1></h1>
-                        </div>
-                        <div class="details">
-                            <div class="approved">
-                                <img src=".approved.jpg" alt="" />
-                            </div>
-                            <div class="text">
-                                <span>205/55 R16 H</span>
-                                <span>SUV</span>
-                            </div>
-                            <div class="text">
-                                <div class="icon">
-                                    <span>Stock </span>
-                                    <img src="./green.png" alt="" />
-                                </div>
-                                <span>Price ₪340</span>
-                            </div>
-                            <div class="text">
-                                <span>Total</span>
-                                <span class="bold">₪1360</span>
-                            </div>
-                        </div>
-                        <div class="quantity">
-                            <div class="counter">
-                                <span class="incre">-</span>
-                                <span class="number">4</span>
-                                <span class="incre">+</span>
-                            </div>
+const ProductHero = (props) => {
 
-                            <div class="adc">
-                                <img src="./adc.png" id="adc" alt="" />
-                                <span>Add to cart</span>
+    const { name, id, stock, price, approved, vehicleType } = props.product
+    const { quantity, total } = props;
+    return (
+        <div class="main">
+            <div class="img">
+                <img src="./tyre.jpg" alt="tyre" id="main-img" />
+            </div>
+            <div class="info">
+                <div class="container">
+                    <div class="heading">
+                        <h1>{name}</h1>
+                    </div>
+                    <div class="details">
+                        <div class="approved">
+                            {
+                                approved ?
+                                    <img src="approved.jpg" alt="img_src" />
+                                    :
+                                    'Not Approved'
+                            }
+                        </div>
+                        <div class="text">
+                            <span>{id}</span>
+                            <span>{vehicleType}</span>
+                        </div>
+                        <div class="text">
+                            <div class="icon">
+                                <span>{props.stockRemaining(stock)} </span>
                             </div>
+                            <span>Price ₪{price}</span>
+                        </div>
+                        <div class="text">
+                            <span>Total</span>
+                            <span class="bold">₪{total}</span>
+                        </div>
+                    </div>
+                    <div class="quantity">
+                        <div class="counter">
+                            <span class="incre" onClick={() => props.decrement()}>-</span>
+                            <span class="number">{quantity}</span>
+                            <span class="incre" onClick={() => props.increment()}>+</span>
+                        </div>
+
+                        <div class="adc">
+                            <img src="./adc.png" id="adc" alt="" />
+                            <span>Add to cart</span>
                         </div>
                     </div>
                 </div>
             </div>
-        
+        </div>
+
     );
 }
 
